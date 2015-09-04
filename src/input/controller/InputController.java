af7b44482aa31bc4a248595c1df95f4d6a5d6dd8
RanchBean <ethr5627@740s-pl-111.local>
2015-09-04 13:26:20 -0600

    Added Code to Repo
    First Code Pushed

---
+++ src/input/controller/InputController.java
@@ -0,0 +1,40 @@
+package input.controller;
+
+import java.util.Scanner;
+
+public class InputController
+{
+	//Declaration
+	//Class members private
+	
+	private Scanner myInput;
+	
+	//Constructors go here
+
+	public InputController()
+	{
+		myInput = new Scanner(System.in);
+	}
+	
+	//Methods
+	
+	public void start()
+	{
+		questions();
+	}
+	private void questions()
+	{
+		System.out.println("What is your name?");
+		String input;
+		input = myInput.nextLine();
+		System.out.println("Hi my name is " + input +",");
+		System.out.println("What is your name");
+		String fullName = myInput.nextLine();
+		System.out.println("My name is, " + fullName);
+		System.out.println("your age please");
+		int myAge = myInput.nextInt();
+		System.out.println("Wow " + input + "you are " + myAge + " years old.");
+	}
+	
+	{
+}
